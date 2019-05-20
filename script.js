a4ea7dec7beb9b570be8d16b8c440f7c449bcec1
Nick <kolyapvp@yandex.ru>
2019-05-21 00:41:11 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+
+// console.log(4/0);
+// console.log('string'+9)
+// let some;
+// console.log(some)
+// let persone = {
+//     name: "Nick",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone["name"]);
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[2])
+//alert("Hello World!");
+// let answer = confirm("Are you her?");
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18 лет ?", "Да");
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + + " - object");
+
+
+let incr = 10,
+    dect = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
